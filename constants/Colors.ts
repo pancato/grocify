55dddc80bfd37d37983d7059d31d63262172493d
pancato <pancat.ooo@gmail.com>
2026-03-20 22:10:06 +0800

    feat: Implement initial Clerk-based authentication with sign-in and sign-up pages, protected routes, and basic theming components.

---
+++ constants/Colors.ts
@@ -0,0 +1,28 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [NativeWind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [Unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+    border: '#E8E8E8',
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+    border: '#333333',
+  },
+};
--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,14 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
     ignores: ['dist/*'],
+    rules: {
+      'import/no-unresolved': 'off'
+    }
   },
 ]);
