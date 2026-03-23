b41e7532a0739977ac5816667c0e2c1a5a33f4ec
pancato <pancat.ooo@gmail.com>
2026-03-23 20:45:39 +0800

    feat: Implement grocery list management with a Zustand store, dedicated API endpoints, and new UI components.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,22 @@
+import { useGroceryStore } from "@/store/grocery-store";
 import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
 import { NativeTabs } from "expo-router/unstable-native-tabs";
+import { useColorScheme } from "nativewind";
+import { useEffect } from "react";
 
-export default function Layout() {
+export default function TabsLayout() {
   const { isSignedIn, isLoaded } = useAuth();
+
+  const { loadItems } = useGroceryStore();
+
+  const { colorScheme } = useColorScheme();
+  const isDark = colorScheme === "dark";
+  const tabTintColor = isDark ? "hsl(142 70% 54%)" : "hsl(147 75% 33%)";
+
+  useEffect(() => {
+    loadItems();
+  }, []);
 
   if (!isLoaded) {
     return null;
@@ -14,18 +27,32 @@ export default function Layout() {
   }
 
   return (
-    <NativeTabs>
+    <NativeTabs tintColor={tabTintColor}>
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>List</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="list.bullet.clipboard" md="list" />
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "list.bullet.clipboard",
+            selected: "list.bullet.clipboard.fill",
+          }}
+          md="list"
+        />
       </NativeTabs.Trigger>
+
       <NativeTabs.Trigger name="planner">
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "plus.circle", selected: "plus.circle.fill" }}
+          md="add"
+        />
         <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="plus.circle" md="add" />
       </NativeTabs.Trigger>
+
       <NativeTabs.Trigger name="insights">
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "chart.bar", selected: "chart.bar.fill" }}
+          md="analytics"
+        />
         <NativeTabs.Trigger.Label>Insights</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="chart.bar" md="analytics" />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
